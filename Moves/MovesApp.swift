03005309c73b21b046587f39ef2f74fd0a3c6428
rijul-newalkar <rijul.ne@gmail.com>
2024-04-30 23:34:08 -0400

    Initial Commit

---
+++ Moves/MovesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovesApp.swift
+//  Moves
+//
+//  Created by Rijul Newalkar on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MovesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
